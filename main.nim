9c320e5c1fa55617a3b4d7fb9128ff10c976029a
muxueqz <zhangmingyuan240@gmail.com>
2019-08-15 17:12:07 +0800

    first commit

---
+++ main.nim
@@ -0,0 +1,59 @@
+import times
+import strutils
+import std/sha1
+import jester
+
+settings:
+  port = Port(5003)
+  bindAddr = "127.0.0.1"
+
+type
+  Block = object
+    Index, BPM: int
+    Timestamp, Hash, PrevHash: string
+
+var Blockchain: seq[Block]
+
+proc calculateHash(b: Block): string =
+  var
+    record = intToStr(b.Index) & b.Timestamp & intToStr(b.BPM) & b.PrevHash
+  return $secureHash(record)
+
+proc generateBlock(oldBlock: Block, BPM: int): Block =
+    var
+      newBlock: Block
+    newBlock.Index = oldBlock.Index + 1
+    newBlock.Timestamp = $getTime().toUnix()
+    newBlock.BPM = BPM
+    newBlock.PrevHash = oldBlock.Hash
+    newBlock.Hash = calculateHash(newBlock)
+    return newBlock
+
+proc isBlockValid(newBlock, oldBlock: Block): bool =
+  if oldBlock.Index+1 != newBlock.Index:
+    return false
+  if oldBlock.Hash != newBlock.PrevHash:
+    return false
+  if calculateHash(newBlock) != newBlock.Hash:
+    return false
+  return true
+
+var
+  genesisBlock: Block
+genesisBlock.Index = 0
+genesisBlock.Timestamp = $getTime().toUnix()
+genesisBlock.BPM = 0
+genesisBlock.PrevHash = ""
+genesisBlock.Hash = ""
+Blockchain.add genesisBlock
+
+routes:
+  get "/":
+    resp $Blockchain
+  post "/":
+    var
+      bpm = @"BPM"
+      newBlock = generateBlock(Blockchain[len(Blockchain)-1], bpm.parseInt)
+    if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]):
+        Blockchain.add newBlock
+    resp Http201, $newBlock
